5df34ecac435d706cffb7494220fc2051b5b8d0b
Carl Molina <carlandrew.molina@gmail.com>
2024-01-04 15:10:12 -0800

    Implemented working delete route fn for companies

--- routes/companies.js
+++ routes/companies.js
@@ -70,12 +70,18 @@ router.post('/', async function(req, res, next) {
         throw BadRequestError("Please enter a code, name, and a description.");
   }
 
-  const result = await db.query(
-    `INSERT INTO companies (code, name, description)
-      values ($1, $2, $3)
-      RETURNING code, name, description`,
-      [code, name, description], // fun fact: this is a trailing comma
-  );
+  let result;
+  try {
+    result = await db.query(
+      `INSERT INTO companies (code, name, description)
+        values ($1, $2, $3)
+        RETURNING code, name, description`,
+        [code, name, description], // fun fact: this is a trailing comma
+    );
+  } catch (err) {
+    // console.log(err);
+    throw new BadRequestError("Company code already in use.");
+  }
 
   const company = result.rows[0];
 
@@ -98,19 +104,73 @@ router.put('/:code', async function(req, res, next) {
 
   const { name, description } = req.body;
 
-  const result = await db.query(
-    `UPDATE companies
-      SET name = $1,
-          description = $2
-      WHERE code = $3
-      RETURNING code, name, description`,
-      [name, description, req.params.code],
-  );
+  if (name === undefined ||
+    description === undefined) {
+      throw BadRequestError("Please enter a code, name, and a description.");
+}
+
+let result;
+  try {
+    result = await db.query(
+      `UPDATE companies
+        SET name = $1,
+            description = $2
+        WHERE code = $3
+        RETURNING code, name, description`,
+        [name, description, req.params.code],
+    );
+  } catch (err) {
+    throw new BadRequestError("Duplicate key value violates unique constraint.");
+    // Placeholder text for now ^
+  }
+
+  if (result.rows.length === 0) {
+    throw new NotFoundError(`Company code ${code} not found in database.`);
+  }
 
   const company = result.rows[0];
 
   return res.json({ company: company });
 
+})
+
+/** DELETE /companies/[code]
+ *
+ *  Deletes company.
+ *
+ *  Returns {status: "deleted"}
+ *
+ *  Should return 404 if company cannot be found.
+ */
+router.delete('/:code', async function (req, res, next) {
+  // console.log('req.params.code:', req.params.code);
+  // if (req.params.code === undefined) throw new BadRequestError();
+
+  const code = req.params.code;
+
+  let result;
+  try {
+    result = await db.query(
+      `DELETE FROM companies
+        WHERE code = $1
+        RETURNING code, name, description`,
+        [req.params.code],
+    );
+  } catch (err) {
+    console.log('err', err);
+    throw new BadRequestError("Could not delete company.");
+    // Placeholder text for now ^
+  }
+
+  console.log('This is result', result);
+  console.log('This is result.rows', result.rows);
+  console.log('This is result.rows.length', result.rows.length);
+
+  if (result.rows.length === 0) {
+    throw new NotFoundError(`Company code ${code} not found in database.`);
+  }
+
+  return res.json({ status: "deleted" });
 })
 
 
